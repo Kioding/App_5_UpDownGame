f5805fc9fc39e63271ee816ebf8b8ae22c8e0794
kio <un19451024@gmail.com>
2021-10-06 16:12:19 +0900

    Feat: ViewController에 스토리보드 Label 및 Button 연결

--- UpDownGame/UpDownGame/ViewController.swift
+++ UpDownGame/UpDownGame/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    // 컴퓨터가 선택한 랜덤 숫자
+    var computerChoice = Int.random(in: 0...10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 1. 메인레이블에 "선택하세요" 표시
+        // 2. 숫자레이블은 "" 표시
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        // 1. 버튼의 숫자를 가져오기
+        // 2. 가져온 숫자 레이블에 표시하기
+        // 3. 선택한 숫자를 변수에 저장
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+        // 1. 컴퓨터의 숫자와 내가 선택한 숫자를 비교하여 메인레이블에 UP / DOWN / Bingo 표시
+        // 2.
+    }
+ 
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        // 1. 메인레이블 "선택하세요" 표시
+        // 2. 컴퓨터의 랜덤 숫자를 다시 선택
+        // 3. 숫자레이블을 "" (빈문자열)
+        
+    }
+    
+    
 }
 
